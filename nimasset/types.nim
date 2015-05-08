325f8fcb90224889474ed8ca97162f523e885399
SSPkrolik <rostislav.dzinko@gmail.com>
2015-05-08 13:35:59 +0300

    Removed unneded non-ref variant of mesh data holder

--- nimasset/types.nim
+++ nimasset/types.nim
@@ -1,25 +1,16 @@
 type
-    MeshDataObj*[V, F] = object
+    MeshData*[V, F] = ref object of RootObj
         vertices: seq[array[0..2, V]]
         faces: seq[array[0..2, F]]
-
-    MeshData*[V, F] = ref MeshDataObj[V, F]
-
 
 proc newMeshData*[V, F](verticesCount: Natural, facesCount: Natural): MeshData[V, F] =
     result.new
     result.vertices = newSeq[V](verticesCount /% 3)
     result.faces = newSeq[F](facesCount /% 3 - 1)
 
-proc `$`*[V, F](mesh: MeshDataObj[V, F]): string =
-    return "string rep of meshdata object"
-
 proc `$`*[V, F](mesh: ref MeshData[V, F]): string =
     return "string rep of meshdata"
 
-
 when (isMainModule):
-    let meshObj = MeshDataObj[float32, int32]()
-    echo meshObj
     let mesh = MeshData[float32, int32].new
     echo mesh
