516c54ad79be12db3f561f525fb0e9b94ea999f9
SSPkrolik <rostislav.dzinko@gmail.com>
2015-05-08 13:21:10 +0300

    Added stringification for object variant of mesh data types

--- nimasset/types.nim
+++ nimasset/types.nim
@@ -11,10 +11,15 @@ proc newMeshData*[V, F](verticesCount: Natural, facesCount: Natural): MeshData[V
     result.vertices = newSeq[V](verticesCount /% 3)
     result.faces = newSeq[F](facesCount /% 3 - 1)
 
+proc `$`*[V, F](mesh: MeshDataObj[V, F]): string =
+    return "string rep of meshdata object"
+
 proc `$`*[V, F](mesh: ref MeshData[V, F]): string =
     return "string rep of meshdata"
 
 
 when (isMainModule):
+    let meshObj = MeshDataObj[float32, int32]()
+    echo meshObj
     let mesh = MeshData[float32, int32].new
     echo mesh
